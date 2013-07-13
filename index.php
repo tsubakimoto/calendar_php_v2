4ef1e0fc9d1dd684b20473b127138fe3e795ceef
Yuta Matsumura <dev.tsubaki@gmail.com>
2013-07-13 10:52:03 +0900

    added index.php, style.css

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>PHPでカレンダー</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<table>
+		<thead>
+			<tr>
+				<th><a href="">&laquo;</a></th>
+				<th colspan="5">2013-07</th>
+				<th><a href="">&raquo;</a></th>
+			</tr>
+			<tr>
+				<th>日</th>
+				<th>月</th>
+				<th>火</th>
+				<th>水</th>
+				<th>木</th>
+				<th>金</th>
+				<th>土</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td class="youbi_0">1</td>
+				<td class="youbi_1">2</td>
+				<td class="youbi_2">3</td>
+				<td class="youbi_3">4</td>
+				<td class="youbi_4">5</td>
+				<td class="youbi_5">6</td>
+				<td class="youbi_6">7</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
